151382e81c36fcd73e3ff2701fe0b887ffa5931c
schmiererl <km228@stud.uni-heidelberg.de>
2024-01-06 18:09:00 +0100

    Added credentials.txt, added user input: expiration date + path to pdf_folder, added IP_email.json

--- secret_functions.py
+++ secret_functions.py
@@ -1,16 +1,30 @@
 from onetimesecret import OneTimeSecretCli
 import secrets
+import os
 
-# The function generates a random password with a standard length of 12 characters #TODO 12?
+# The function reads One-Time-Secret credentials from the credentials.txt file
+def read_onetimesecret_credentials():
+    with open('credentials.txt', 'r') as file:
+        lines = file.readlines()
+        credentials = {}
+        for line in lines:
+            key, value = line.strip().split('=')
+            credentials[key] = value
+        return credentials
+
+# The function generates a random password with a default length of 12 characters
 def generate_random_password(length=12):
     return secrets.token_urlsafe(length)
 
-# The function "create_one_time_secret_link" creates a one-time secret link for the submitted secret 
-# with a specific email, API key and custom URL for Heidelberg University's One-Time Secret service
+# The function creates a one-time secret link for the submitted secret 
+# with a specific email, API key, and custom URL for Heidelberg University's One-Time Secret service
 def create_one_time_secret_link(secret):
-    email = "EMAIL"  
-    api_key = "API_KEY"  
-    custom_url = "https://onetimesecret.urz.uni-heidelberg.de:443"  
+    # Read One-Time-Secret credentials from the credentials.txt file
+    credentials = read_onetimesecret_credentials()
+
+    email = credentials['email']
+    api_key = credentials['api_key']
+    custom_url = credentials['custom_url']
 
     oneTimeSecretCli = OneTimeSecretCli(email, api_key, url=custom_url)
     return oneTimeSecretCli.create_link(secret)
