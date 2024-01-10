1485814e788c9ede771197e960678b1188942c5b
schmiererl <km228@stud.uni-heidelberg.de>
2024-01-10 10:52:41 +0100

    updated create_flag and credentials for otp

--- secret_functions.py
+++ secret_functions.py
@@ -13,7 +13,6 @@ def read_onetimesecret_credentials():
             return credentials
     except Exception as e:
         print(f"Error reading One-Time-Secret credentials: {str(e)}")
-        
 
 # The function generates a random password with a default length of 12 characters
 def generate_random_password(length=12):
@@ -21,28 +20,24 @@ def generate_random_password(length=12):
         return secrets.token_urlsafe(length)
     except Exception as e:
         print(f"Error generating random password: {str(e)}")
-        
 
 # The function creates a one-time secret link for the submitted secret 
-# with custom URL for Heidelberg University's One-Time Secret service
+# with a custom URL for Heidelberg University's One-Time Secret service
 def create_one_time_secret_link(secret):
     try:
         # Read One-Time-Secret credentials from the credentials.txt file
         credentials = read_onetimesecret_credentials()
-        otp_api_url = credentials['otp_api_url']
-        otp_link = credentials['otp_link']
+        otp_domain = credentials['otp_domain']
 
         # Send request to create the one-time secret to the One-Time-Secret API
-        response = requests.post(otp_api_url, data={'secret': secret})
+        response = requests.post(otp_domain + '/api/v1/share', data={'secret': secret})
         response_data = response.json()
 
         secret_key = response_data['secret_key']
 
         # Create the one-time secret link
-        link = otp_link+secret_key
+        link = otp_domain + '/secret/' + secret_key
         return link
-        
 
     except Exception as e:
         print(f"Error creating One-Time-Secret link: {str(e)}")
-        
